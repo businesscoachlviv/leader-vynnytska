629e779a0716467b104e66fab8b1036c29154ebf
businesscoachlviv <expertbloglviv@gmail.com>
2026-09-22 23:10:40 +0300

    Add files via upload

---
+++ _ds_bundle.js
@@ -0,0 +1,11 @@
+/* @ds-bundle: {"format":4,"namespace":"BusinessCoachDesignSystem_28e7c2","components":[],"sourceHashes":{},"inlinedExternals":[],"unexposedExports":[]} */
+
+(() => {
+
+const __ds_ns = (window.BusinessCoachDesignSystem_28e7c2 = window.BusinessCoachDesignSystem_28e7c2 || {});
+
+const __ds_scope = {};
+
+(__ds_ns.__errors = __ds_ns.__errors || []);
+
+})();
